831e2ee2df3b53dbc72e60c7f90a2a3291b7f9d3
nick3842 <nick3842@protonmail.com>
2022-05-15 20:53:42 -0500

    Add server side rendering

--- pages/cars/[id].js
+++ pages/cars/[id].js
@@ -15,22 +15,30 @@ export default function Car({ car }) {
   </>)
 }
 
-export async function getStaticProps({ params }) {
+// export async function getStaticProps({ params }) {
+//   const req = await fetch(`http://localhost:3000/${params.id}.json`)
+//   const data = await req.json()
+//   return {
+//     props: { car: data }
+//   }
+// }
+
+// export async function getStaticPaths() {
+//   const req = await fetch('http://localhost:3000/cars.json')
+//   const data = await req.json()
+//   const paths = data.map(car => {
+//     return { params: { id: car } }
+//   })
+//   return {
+//     paths,
+//     fallback: false
+//   }
+// }
+// serversideprops and static paths not compatible
+export async function getServerSideProps({ params }) {
   const req = await fetch(`http://localhost:3000/${params.id}.json`)
   const data = await req.json()
   return {
     props: { car: data }
-  }
-}
-
-export async function getStaticPaths() {
-  const req = await fetch('http://localhost:3000/cars.json')
-  const data = await req.json()
-  const paths = data.map(car => {
-    return { params: { id: car } }
-  })
-  return {
-    paths,
-    fallback: false
   }
 }
