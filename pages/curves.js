c2789c2a89016fac045f2f8a9f6494d24fcad238
nick3842 <nick3842@protonmail.com>
2022-05-17 21:14:39 -0500

    Working on adding curves page

---
+++ pages/curves.js
@@ -0,0 +1,48 @@
+const Text = () => {
+  return (
+    <body>
+      <section>
+        <h1>Nice Curves</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate maiores sunt dolore culpa assumenda, amet dolor in nam, minima quasi obcaecati non voluptas quas aperiam quisquam unde facilis, accusantium doloremque?</p>
+      </section>
+      <section>
+        <h1>Nice Curves</h1>
+        <p>Neque eaque rerum, adipisci commodi nesciunt consectetur suscipit, nisi perspiciatis odit illo inventore obcaecati saepe ipsa ab hic quaerat eum eos aperiam! Recusandae at debitis veritatis eos dolores harum explicabo.</p>
+      </section>
+      <section>
+        <h1>Nice Curves</h1>
+        <p>Eum dolore porro temporibus non illo! Similique unde beatae nulla natus expedita nobis inventore fugiat nihil eum dignissimos, magnam facilis explicabo voluptatem voluptas consectetur dolores provident qui harum possimus reprehenderit.</p>
+      </section>
+      <section>
+        <h1>Nice Curves</h1>
+        <p>Esse dolorem porro doloribus suscipit! Perferendis asperiores optio earum nostrum rem rerum nobis doloremque excepturi cupiditate beatae aperiam ducimus corrupti, odit sunt numquam nam provident necessitatibus aspernatur tempore dolores dolore?</p>
+      </section>
+      <section>
+        <h1>Nice Curves</h1>
+        <p>Libero nobis veritatis eaque dicta blanditiis temporibus tempore error reprehenderit quaerat deleniti suscipit neque, porro dolor, unde, doloremque nisi sed. Error voluptates quod nobis, deleniti recusandae cumque hic perspiciatis dolorum!</p>
+      </section>
+      <section>
+        <h1>Nice Curves</h1>
+        <p>Fuga blanditiis dolor corporis doloribus. Veritatis ex magni esse quasi exercitationem minus, blanditiis nobis quidem similique, sit voluptas repellendus? Sint aliquam impedit excepturi earum voluptate iure in recusandae deserunt enim?</p>
+      </section>
+      <section>
+        <h1>Nice Curves</h1>
+        <p>Iusto laborum facilis veniam quos consequuntur necessitatibus voluptatibus nesciunt eius officiis, aut omnis non obcaecati eveniet distinctio praesentium expedita nemo debitis dolor nulla hic blanditiis accusamus. In temporibus reiciendis porro.</p>
+      </section>
+      <section>
+        <h1>Nice Curves</h1>
+        <p>Voluptates temporibus quis consequuntur recusandae architecto totam eos. Ipsam et, omnis necessitatibus delectus voluptates voluptatibus, id tenetur provident quisquam atque quae iste sapiente dolorem similique. Sapiente dolorem praesentium neque quo.</p>
+      </section>
+    </body>
+  )
+}
+
+const Curves = () => {
+  return (
+    <div>
+      <Text />
+    </div>
+  )
+}
+
+export default Curves
